01701b006da246ed9c873040d85c3d00ba3cb4bf
mati <matias.burgos.c@mail.pucv.cl>
2023-06-10 21:27:56 -0400

    proyectos con json

--- src/pages/Proyectos.jsx
+++ src/pages/Proyectos.jsx
@@ -1,17 +1,24 @@
 import { useEffect, useState } from "react";
 
+import Vista from "../components/VistaProyecto";
 import data from "../data/proyectos.json";
+
+import styles from "../styles/modules/Proyectos.module.scss";
 
 function Proyectos() {
     return (
         <div>
-            <main className="p-5">
-                <h1 className="display-6">Proyectos</h1>
-                <hr />
-                <div className="album py-5 bg-light">
+            <main className={`${styles.main} p-5`}>
+                <h1 className="d-flex justify-content-center">Proyectos</h1>
+                <hr className={styles.salto} />
+                <div className={`${styles.caja} album py-5 bg-light`}>
                     <div className="container">
-                        <div className="row row-cols-1 row-cols-sm-2 row-cols-md-3 row-cols-lg-4 row-cols-xl-5 g-3">
-                            hola
+                        <div className="row row-cols-1 row-cols-sm-1 row-cols-md-2 row-cols-lg-3 row-cols-xl-4 g-3 ">
+                            {data.map((proyecto) => (
+                                <div className="col" key={proyecto.id}>
+                                    <Vista proyecto={proyecto} />
+                                </div>
+                            ))}
                         </div>
                     </div>
                 </div>
