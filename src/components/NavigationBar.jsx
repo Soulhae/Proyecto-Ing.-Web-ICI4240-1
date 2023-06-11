bd2ac22f77db2de4a0ea904f8bd5ce9376df123e
nikobinho <nico.barraza.a14@gmail.com>
2023-06-10 21:54:11 -0400

    form

--- src/components/NavigationBar.jsx
+++ src/components/NavigationBar.jsx
@@ -1,55 +1,46 @@
-import {
-    Navbar,
-    Nav,
-    Form,
-    Button,
-    Container,
-} from "react-bootstrap";
+import { Navbar, Nav, Form, Button, Container } from "react-bootstrap";
 
 function NavigationBar() {
-    return (
-        <Navbar bg="light" expand="lg">
-            <Container
-                fluid
-                style={{
-                    color: "#363636",
-                    fontWeight: 700,
-                }}
-            >
-                <Navbar.Brand href="/">Nimabe</Navbar.Brand>
-                <Navbar.Toggle aria-controls="navbarScroll" />
-                <Navbar.Collapse id="navbarScroll">
-                    <Nav
-                        className="me-auto my-2 my-lg-0"
-                        style={{ maxHeight: "100px" }}
-                        navbarScroll
-                    >
-                        <Nav.Link href="/nuevo_proyecto">
-                            Inicia tu proyecto
-                        </Nav.Link>
-                        <Nav.Link href="/proyectos">Proyectos</Nav.Link>
-                        <Nav.Link href="/nosotros">Nosotros</Nav.Link>
-                        <Nav.Link href="/faq">FAQ</Nav.Link>
-                    </Nav>
-                    <Form className="d-flex">
-                        <Form.Control
-                            type="search"
-                            placeholder="Buscar"
-                            className="me-2"
-                            aria-label="Search"
-                        />
-                        <Button variant="outline-success" href="/busqueda">Buscar</Button>
-                    </Form>
-                    <Nav
-                        style={{ maxHeight: "100px" }}
-                        navbarScroll
-                    >
-                        <Nav.Link href="/login">Iniciar Sesión</Nav.Link>
-                    </Nav>
-                </Navbar.Collapse>
-            </Container>
-        </Navbar>
-    );
+  return (
+    <Navbar bg="light" expand="lg">
+      <Container
+        fluid
+        style={{
+          color: "#363636",
+          fontWeight: 700,
+        }}
+      >
+        <Navbar.Brand href="/">Nimabe</Navbar.Brand>
+        <Navbar.Toggle aria-controls="navbarScroll" />
+        <Navbar.Collapse id="navbarScroll">
+          <Nav
+            className="me-auto my-2 my-lg-0"
+            style={{ maxHeight: "100px" }}
+            navbarScroll
+          >
+            <Nav.Link href="/nuevo_proyecto">Inicia tu proyecto</Nav.Link>
+            <Nav.Link href="/proyectos">Proyectos</Nav.Link>
+            <Nav.Link href="/nosotros">Nosotros</Nav.Link>
+            <Nav.Link href="/faq">FAQ</Nav.Link>
+          </Nav>
+          <Form className="d-flex">
+            <Form.Control
+              type="search"
+              placeholder="Buscar"
+              className="me-2"
+              aria-label="Search"
+            />
+            <Button variant="outline-success" href="/busqueda">
+              Buscar
+            </Button>
+          </Form>
+          <Nav style={{ maxHeight: "100px" }} navbarScroll>
+            <Nav.Link href="/login">Iniciar Sesión</Nav.Link>
+          </Nav>
+        </Navbar.Collapse>
+      </Container>
+    </Navbar>
+  );
 }
 
 export default NavigationBar;
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,7 +1,70 @@
 
-
 function Login() {
-    return <div>Login</div>;
+  return (
+    <div
+      className="register"
+      style={{
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "#f8ffa9",
+      }}
+    >
+      <div
+        className="box"
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          backgroundColor: "#D9D9D9",
+          borderRadius: "10px",
+          boxShadow: "0px 0px 10px 0px rgba(0, 0, 0, 0.5)",
+          maxHeight: "500px",
+          maxWidth: "300px",
+          padding: "20px",
+          margin: "30px",
+        }}
+      >
+        <h2>Iniciar sesion</h2>
+
+        <form>
+          <div className="form-group mb-2">
+            <label htmlFor="email" classname="form-label">
+              Email
+            </label>
+            <input type="email" className="form-control" required />
+          </div>
+          <div className="form-group mb-2">
+            <label htmlFor="password" classname="form-label">
+              Contraseña
+            </label>
+            <input type="password" className="form-control" required />
+              Por favor ingrese su contraseña
+          </div>
+          <div className="form-group form-check">
+            <input type="checkbox" className="form-check-input" />
+            <label htmlFor="check" className="form-check-label">
+              Recuerdame
+            </label>
+          </div>
+        </form>
+
+        <button
+          type="submit"
+          className="btn btn primary block mt-2 w-100"
+          style={{
+            backgroundColor: "#f8ffa9",
+            fontSize: 15,
+            color: "black",
+          }}
+        >
+          Iniciar sesion
+        </button>
+      </div>
+    </div>
+  );
 }
 
 export default Login;
