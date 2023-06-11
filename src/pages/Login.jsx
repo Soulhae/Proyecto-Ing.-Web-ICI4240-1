7f8d6ff021f58a85c54603163469cc06c685b614
mati <matias.burgos.c@mail.pucv.cl>
2023-06-11 14:55:35 -0400

    login register route

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,82 +1,110 @@
-import styles from "../styles/modules/Login.module.scss"
+import styles from "../styles/modules/Login.module.scss";
 import { useFormik } from "formik";
 import { lSchema } from "../schemas/indexL";
 import { Container } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
-const onSubmit = async (values ,actions) => { 
-
-  console.log(values);
-  console.log(actions);
-  await new Promise((resolve) => setTimeout(resolve,1000));
-  actions.resetForm();
+const onSubmit = async (values, actions) => {
+    console.log(values);
+    console.log(actions);
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    actions.resetForm();
 };
-
 
 const Login = () => {
-  const { values, errors, touched, isSubmitting,handleBlur, handleChange, handleSubmit} = useFormik({
-    initialValues: {
-   email:"",
-   password:"",
-   
-    },
-    validationSchema: lSchema,
-    onSubmit,
-  });
+    const {
+        values,
+        errors,
+        touched,
+        isSubmitting,
+        handleBlur,
+        handleChange,
+        handleSubmit,
+    } = useFormik({
+        initialValues: {
+            email: "",
+            password: "",
+        },
+        validationSchema: lSchema,
+        onSubmit,
+    });
 
-  console.log(errors);
+    console.log(errors);
 
-  return (
-    <Container>
-      <div className={`m-auto my-5 ${styles.boxL}`}>
-        <form onSubmit={handleSubmit} autoComplete="off">
-          <label
-            htmlFor="email"
-            style={{
-              fontsize: "1rem",
-              fontweight: "bold",
-              display: "block",
-              textalign: "left",
-              margin: "1rem 0 0.2rem",
-            }}
-          >
-            Email
-          </label>
-          <input
-            id="email"
-            type="email"
-            placeholder="Ingrese su email"
-            value={values.email}
-            onChange={handleChange}
-            onBlur={handleBlur}
-            className={errors.email && touched.email? `${styles.inputerror}` : ""}
-          ></input>{errors.email && touched.email && <p className={`${styles.errorMsg}`}>{errors.email}</p>}
-          <label
-            htmlFor="password"
-            style={{
-              fontsize: "1rem",
-              fontweight: "bold",
-              display: "block",
-              textalign: "left",
-              margin: "1rem 0 0.2rem",
-            }}
-          >
-            Contraseña
-          </label>
-          <input
-            id="password"
-            type="password"
-            placeholder="Ingrese una contraseña"
-            value={values.password}
-            onChange={handleChange}
-            onBlur={handleBlur}
-            className={errors.password && touched.password? `${styles.inputerror}` : ""}
-          ></input>{errors.password && touched.password && <p className={`${styles.errorMsg}`}>{errors.password}</p>}
-          <button disabled={isSubmitting} type="submit">Iniciar sesion</button>
-        </form>
-      </div>
-    </Container>
-  );
+    return (
+        <Container>
+            <div className={`m-auto my-5 ${styles.boxL}`}>
+                <form onSubmit={handleSubmit} autoComplete="off">
+                    <h1>Iniciar sesión</h1>
+                    <label
+                        htmlFor="email"
+                        style={{
+                            fontsize: "1rem",
+                            fontweight: "bold",
+                            display: "block",
+                            textalign: "left",
+                            margin: "1rem 0 0.2rem",
+                        }}
+                    >
+                        Email
+                    </label>
+                    <input
+                        id="email"
+                        type="email"
+                        placeholder="Ingrese su email"
+                        value={values.email}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        className={
+                            errors.email && touched.email
+                                ? `${styles.inputerror}`
+                                : ""
+                        }
+                    ></input>
+                    {errors.email && touched.email && (
+                        <p className={`${styles.errorMsg}`}>{errors.email}</p>
+                    )}
+                    <label
+                        htmlFor="password"
+                        style={{
+                            fontsize: "1rem",
+                            fontweight: "bold",
+                            display: "block",
+                            textalign: "left",
+                            margin: "1rem 0 0.2rem",
+                        }}
+                    >
+                        Contraseña
+                    </label>
+                    <input
+                        id="password"
+                        type="password"
+                        placeholder="Ingrese una contraseña"
+                        value={values.password}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        className={
+                            errors.password && touched.password
+                                ? `${styles.inputerror}`
+                                : ""
+                        }
+                    ></input>
+                    {errors.password && touched.password && (
+                        <p className={`${styles.errorMsg}`}>
+                            {errors.password}
+                        </p>
+                    )}
+                    <button disabled={isSubmitting} type="submit">
+                        Iniciar sesion
+                    </button>
+                    <hr />
+                    <p>
+                        ¿Nuevo usuario? <Link to="/register">¡Registrate!</Link>
+                    </p>
+                </form>
+            </div>
+        </Container>
+    );
 };
-
 
 export default Login;
--- src/pages/Register.jsx
+++ src/pages/Register.jsx
@@ -1,156 +1,215 @@
-import styles from "../styles/modules/Register.module.scss"
+import styles from "../styles/modules/Register.module.scss";
 import { useFormik } from "formik";
 import { basicSchema } from "../schemas/index";
 import { Container } from "react-bootstrap";
-import axios from 'axios';
+import { Link } from "react-router-dom";
+import axios from "axios";
 
-const onSubmit = async (values ,actions) => { 
-
-  console.log(values);
-  //console.log(actions);
-  axios
-        .post('http://localhost:3000/usuarios', values)
+const onSubmit = async (values, actions) => {
+    console.log(values);
+    //console.log(actions);
+    axios
+        .post("http://localhost:3000/usuarios", values)
         .then((response) => {
-          console.log(response.data);
+            console.log(response.data);
         })
         .catch((error) => {
-          console.error(error);
+            console.error(error);
         });
-  await new Promise((resolve) => setTimeout(resolve,1000));
-  actions.resetForm();
+    await new Promise((resolve) => setTimeout(resolve, 1000));
+    actions.resetForm();
 };
 
-
 const Register = () => {
-  const { values, errors, touched, isSubmitting,handleBlur, handleChange, handleSubmit} = useFormik({
-    initialValues: {
-   email:"",
-   username:"",
-   password:"",
-   confirmPassword:"",
-   number:"",
-    },
-    validationSchema: basicSchema,
-    onSubmit,
-  });
+    const {
+        values,
+        errors,
+        touched,
+        isSubmitting,
+        handleBlur,
+        handleChange,
+        handleSubmit,
+    } = useFormik({
+        initialValues: {
+            email: "",
+            username: "",
+            password: "",
+            confirmPassword: "",
+            number: "",
+        },
+        validationSchema: basicSchema,
+        onSubmit,
+    });
 
-  //console.log(errors);
+    //console.log(errors);
 
-  return (
-    <Container>
-      <div className={`m-auto my-5 ${styles.boxL}`}>
-        <form id="formulario" onSubmit={handleSubmit} autoComplete="off">
-          <label
-            htmlFor="email"
-            style={{
-              fontsize: "1rem",
-              fontweight: "bold",
-              display: "block",
-              textalign: "left",
-              margin: "1rem 0 0.2rem",
-            }}
-          >
-            Email
-          </label>
-          <input
-            id="email"
-            type="email"
-            placeholder="Ingrese su email"
-            value={values.email}
-            onChange={handleChange}
-            onBlur={handleBlur}
-            className={errors.email && touched.email? `${styles.inputerror}` : ""}
-          ></input>{errors.email && touched.email && <p className={`${styles.errorMsg}`}>{errors.email}</p>}
-          <label
-            htmlFor="username"
-            style={{
-              fontsize: "1rem",
-              fontweight: "bold",
-              display: "block",
-              textalign: "left",
-              margin: "1rem 0 0.2rem",
-            }}
-          >
-            Usuario
-          </label>
-          <input
-            id="username"
-            type="username"
-            placeholder="Ingrese un usuario"
-            value={values.username}
-            onChange={handleChange}
-            onBlur={handleBlur}
-            className={errors.username && touched.username? `${styles.inputerror}` : ""}
-          ></input>
-          {errors.username && touched.username && <p className={`${styles.errorMsg}`}>{errors.username}</p>}
-          <label
-            htmlFor="password"
-            style={{
-              fontsize: "1rem",
-              fontweight: "bold",
-              display: "block",
-              textalign: "left",
-              margin: "1rem 0 0.2rem",
-            }}
-          >
-            Contraseña
-          </label>
-          <input
-            id="password"
-            type="password"
-            placeholder="Ingrese una contraseña"
-            value={values.password}
-            onChange={handleChange}
-            onBlur={handleBlur}
-            className={errors.password && touched.password? `${styles.inputerror}` : ""}
-          ></input>{errors.password && touched.password && <p className={`${styles.errorMsg}`}>{errors.password}</p>}
-          <label
-            htmlFor="confirmPassword"
-            style={{
-              fontsize: "1rem",
-              fontweight: "bold",
-              display: "block",
-              textalign: "left",
-              margin: "1rem 0 0.2rem",
-            }}
-          >
-            Reingresar contraseña
-          </label>
-          <input
-            id="confirmPassword"
-            type="password"
-            placeholder="Reingrese la contraseña"
-            value={values.confirmPassword}
-            onChange={handleChange}
-            onBlur={handleBlur}
-            className={errors.confirmPassword && touched.confirmPassword? `${styles.inputerror}` : ""}
-          ></input>{errors.confirmPassword && touched.confirmPassword && <p className={`${styles.errorMsg}`}>{errors.confirmPassword}</p>}
-          <label
-            htmlFor="number"
-            style={{
-              fontsize: "1rem",
-              fontweight: "bold",
-              display: "block",
-              textalign: "left",
-              margin: "1rem 0 0.2rem",
-            }}
-          >
-            Número telefonico
-          </label>
-          <input
-            id="number"
-            type="number"
-            placeholder="Ingrese su número telefonico"
-            value={values.number}
-            onChange={handleChange}
-            onBlur={handleBlur}
-            className={errors.number && touched.number? `${styles.inputerror}` : ""}
-          ></input>{errors.number && touched.number && <p className={`${styles.errorMsg}`}>{errors.number}</p>}
-          <button disabled={isSubmitting} type="submit">Registrarse</button>
-        </form>
-      </div>
-    </Container>
-  );
+    return (
+        <Container>
+            <div className={`m-auto my-5 ${styles.boxL}`}>
+                <form
+                    id="formulario"
+                    onSubmit={handleSubmit}
+                    autoComplete="off"
+                >
+                    <h1>Registrarse</h1>
+                    <label
+                        htmlFor="email"
+                        style={{
+                            fontsize: "1rem",
+                            fontweight: "bold",
+                            display: "block",
+                            textalign: "left",
+                            margin: "1rem 0 0.2rem",
+                        }}
+                    >
+                        Email
+                    </label>
+                    <input
+                        id="email"
+                        type="email"
+                        placeholder="Ingrese su email"
+                        value={values.email}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        className={
+                            errors.email && touched.email
+                                ? `${styles.inputerror}`
+                                : ""
+                        }
+                    ></input>
+                    {errors.email && touched.email && (
+                        <p className={`${styles.errorMsg}`}>{errors.email}</p>
+                    )}
+                    <label
+                        htmlFor="username"
+                        style={{
+                            fontsize: "1rem",
+                            fontweight: "bold",
+                            display: "block",
+                            textalign: "left",
+                            margin: "1rem 0 0.2rem",
+                        }}
+                    >
+                        Usuario
+                    </label>
+                    <input
+                        id="username"
+                        type="username"
+                        placeholder="Ingrese un usuario"
+                        value={values.username}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        className={
+                            errors.username && touched.username
+                                ? `${styles.inputerror}`
+                                : ""
+                        }
+                    ></input>
+                    {errors.username && touched.username && (
+                        <p className={`${styles.errorMsg}`}>
+                            {errors.username}
+                        </p>
+                    )}
+                    <label
+                        htmlFor="password"
+                        style={{
+                            fontsize: "1rem",
+                            fontweight: "bold",
+                            display: "block",
+                            textalign: "left",
+                            margin: "1rem 0 0.2rem",
+                        }}
+                    >
+                        Contraseña
+                    </label>
+                    <input
+                        id="password"
+                        type="password"
+                        placeholder="Ingrese una contraseña"
+                        value={values.password}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        className={
+                            errors.password && touched.password
+                                ? `${styles.inputerror}`
+                                : ""
+                        }
+                    ></input>
+                    {errors.password && touched.password && (
+                        <p className={`${styles.errorMsg}`}>
+                            {errors.password}
+                        </p>
+                    )}
+                    <label
+                        htmlFor="confirmPassword"
+                        style={{
+                            fontsize: "1rem",
+                            fontweight: "bold",
+                            display: "block",
+                            textalign: "left",
+                            margin: "1rem 0 0.2rem",
+                        }}
+                    >
+                        Reingresar contraseña
+                    </label>
+                    <input
+                        id="confirmPassword"
+                        type="password"
+                        placeholder="Reingrese la contraseña"
+                        value={values.confirmPassword}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        className={
+                            errors.confirmPassword && touched.confirmPassword
+                                ? `${styles.inputerror}`
+                                : ""
+                        }
+                    ></input>
+                    {errors.confirmPassword && touched.confirmPassword && (
+                        <p className={`${styles.errorMsg}`}>
+                            {errors.confirmPassword}
+                        </p>
+                    )}
+                    <label
+                        htmlFor="number"
+                        style={{
+                            fontsize: "1rem",
+                            fontweight: "bold",
+                            display: "block",
+                            textalign: "left",
+                            margin: "1rem 0 0.2rem",
+                        }}
+                    >
+                        Número telefonico
+                    </label>
+                    <input
+                        id="number"
+                        type="number"
+                        placeholder="Ingrese su número telefonico"
+                        value={values.number}
+                        onChange={handleChange}
+                        onBlur={handleBlur}
+                        className={
+                            errors.number && touched.number
+                                ? `${styles.inputerror}`
+                                : ""
+                        }
+                    ></input>
+                    {errors.number && touched.number && (
+                        <p className={`${styles.errorMsg}`}>{errors.number}</p>
+                    )}
+                    <button disabled={isSubmitting} type="submit">
+                        Registrarse
+                    </button>
+                    <hr />
+                    <p>
+                        ¿Ya tienes una cuenta?
+                        <Link to="/login">¡Inicia sesión!</Link>
+                    </p>
+                </form>
+            </div>
+        </Container>
+    );
 };
 
 export default Register;
